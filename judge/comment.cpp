1e9a4fe6fc391028b3920452e37c5fc4988f071b
yegorf1 <yegorf1@gmail.com>
2015-04-18 18:04:59 +0300

    Implemented Comment class

--- judge/comment.cpp
+++ judge/comment.cpp
@@ -1,5 +1,12 @@
 #include "comment.h"
 
 Comment::Comment(char* comment) {
+	this->mComment = string(comment);
+}
 
+Comment::~Comment() { }
+
+float Comment::GetRating() {
+
+	return 1.0f;
 }
--- judge/comment.h
+++ judge/comment.h
@@ -1,13 +1,21 @@
 #ifndef COMMENT_H
 #define COMMENT_H
 
+#include <string>
+
 using namespace std;
 
 class Comment {
 public:
 	Comment(char *content);
-
 	~Comment();
+
+	// Returns value form 0 to 1 for getting 'bad' level
+	float GetRating();
+
+private:
+	// Stores original comment string
+	string mComment;
 };
 
 #endif
