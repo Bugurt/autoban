0818a2487196435e640b12cd8e143c691f54f86d
yegorf1 <yegorf1@gmail.com>
2015-04-19 11:50:21 +0300

    Changed default comment and found bug with argc and argv in my enviropment

--- judge/main.cpp
+++ judge/main.cpp
@@ -3,13 +3,17 @@
 using namespace std;
 
 int main (int argc, char *argv[]) {
-	char *comment = (char *)"None";
+	char *comment = (char *)"Hello, world";
 
-	if (argc == 1) {
-		comment = argv[0];
+	// So, there is magic. If I launch it with "./judge"
+	// there will be one arg and argv[0] equals to "judge",
+	// but if I launch "./judge firstArg" argc == 2 and
+	// argv[1] == firstArg. Really strange
+	if (argc == 2) {
+		comment = argv[1];
+	} else if (argc != 1) {
+		cout << "Unexpected args count: " << argc << endl;
 	}
-
-	cout << comment << endl;
 
 	return 0;
 }
