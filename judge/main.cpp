ea59024e9caa35c8d11fb81edac26c613db2193f
yegorf1 <yegorf1@gmail.com>
2015-04-19 13:16:06 +0300

    Remove comment about first arg

--- judge/main.cpp
+++ judge/main.cpp
@@ -5,10 +5,6 @@ using namespace std;
 int main (int argc, char *argv[]) {
 	char *comment = (char *)"Hello, world";
 
-	// So, there is magic. If I launch it with "./judge"
-	// there will be one arg and argv[0] equals to "judge",
-	// but if I launch "./judge firstArg" argc == 2 and
-	// argv[1] == firstArg. Really strange
 	if (argc == 2) {
 		comment = argv[1];
 	} else if (argc != 1) {
